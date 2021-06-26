238ceb083a0447362d016c536c9beb9ea7ed5633
EduardoAnhaia <86513440+EduardoAnhaia@users.noreply.github.com>
2021-06-26 15:44:11 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,41 @@
 # calculadora.js
+function calculadora (valorinvest){
+  
+  var viewsoriginal = (valorinvest * 30);
+  var viewsatual = viewsoriginal
+  var viewstotal = viewsoriginal
+
+
+  
+  for(i=0;i<4;i++){
+   
+  var clicks = viewsatual/8.33333;
+  var compartilhamento = clicks/6.67;
+  var novasviews = compartilhamento * 40;
+  viewsatual = novasviews
+  viewstotal = viewstotal + novasviews
+  
+  if(viewsatual = 0){
+    break
+  }
+
+  }
+  
+  
+  
+  
+return parseInt(viewstotal)
+
+}
+
+
+var valor = 1000000
+var visualizacoes = calculadora(valor)
+
+
+
+console.log("O valor aproximado de visualizações é de " + calculadora(valor) + " Para R$ " + valor.toFixed(2) + " investidos")
+
+
+
+
